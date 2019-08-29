1734ee2e691270e9f1cd0d21f8baad8107f80800
chris-revell <ckrevell@gmail.com>
2019-08-29 16:25:13 +0100

    Reverting to initial layout

--- CharacteriseShape.py
+++ CharacteriseShape.py
@@ -12,10 +12,10 @@ from astropy import modeling
 
 # Function to import image and generate bw image
 def read_cell(file):
-    image = cv2.imread(file)
+    image = cv2.imread(argv[1])
     gray_image = invert(cv2.cvtColor(image, cv2.COLOR_BGR2GRAY))
-    #blur = cv2.GaussianBlur(gray_image, (15, 15), 0)
-    #ret3, bw_image = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    blur = cv2.GaussianBlur(gray_image, (15, 15), 0)
+    ret3, bw_image = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
     bw_image = 255-bw_image
     return image,bw_image
 
@@ -25,79 +25,99 @@ def med_axis(bw_image):
     dist_on_skel = distance * skel/2.0
     return dist_on_skel
 
+# Create black and white image
+originalimage,bw = read_cell(argv[1])
+# Create medial axis from imported image
+maxis = med_axis(bw)
 
-def findLongestRoute(filename):
-    # Create black and white image
-    originalimage,bw = read_cell(filename)
-    # Create medial axis from imported image
-    maxis = med_axis(bw)
+# Array to store the indices of neighbouring pixels within the medial axis for all pixels within the medial axis.
+neighbours = np.zeros((np.shape(maxis)[0],np.shape(maxis)[1],18),dtype='int')
+for x in range(1,np.shape(maxis)[0]-1):
+    for y in range(1,np.shape(maxis)[1]-1):
+        if maxis[x,y] > 0:
+            # if maxis[x,y] > 0 then pixel x,y is part of the medial axis.
+            # Now test all pixels surrounding the current pixel under consideration.
+            for i in range(-1,2):
+                for j in range(-1,2):
+                    if (i,j) != (0,0) and maxis[x+i,y+j]>0:
+                        # 0th component of 3rd dimension stores the number of neighbours pixel x,y has. Remainder of components store the indices of those neighbours.
+                        neighbours[x,y,0] = neighbours[x,y,0]+1
+                        neighbours[x,y,int(2*neighbours[x,y,0]-1)] = x+i
+                        neighbours[x,y,int(2*neighbours[x,y,0])]   = y+j
 
-    # Array to store the indices of neighbouring pixels within the medial axis for all pixels within the medial axis.
-    neighbours = np.zeros((np.shape(maxis)[0],np.shape(maxis)[1],18),dtype='int')
-    for x in range(1,np.shape(maxis)[0]-1):
-        for y in range(1,np.shape(maxis)[1]-1):
-            if maxis[x,y] > 0:
-                # if maxis[x,y] > 0 then pixel x,y is part of the medial axis.
-                # Now test all pixels surrounding the current pixel under consideration.
-                for i in range(-1,2):
-                    for j in range(-1,2):
-                        if (i,j) != (0,0) and maxis[x+i,y+j]>0:
-                            # 0th component of 3rd dimension stores the number of neighbours pixel x,y has. Remainder of components store the indices of those neighbours.
-                            neighbours[x,y,0] = neighbours[x,y,0]+1
-                            neighbours[x,y,int(2*neighbours[x,y,0]-1)] = x+i
-                            neighbours[x,y,int(2*neighbours[x,y,0])]   = y+j
+tips = []
+branches = []
+pseudobranches = []
+for x in range(np.shape(maxis)[0]):
+    for y in range(np.shape(maxis)[1]):
+        if neighbours[x,y,0] == 1:
+            tips.append((x,y))
+        elif neighbours[x,y,0] == 3:
+            notBranch = 0
+            for i in range(3):
+                for j in range(3):
+                    if i==j:
+                        pass
+                    else:
+                        xdif = neighbours[x,y,2*i+1]-neighbours[x,y,2*j+1]
+                        ydif = neighbours[x,y,2*i+2]-neighbours[x,y,2*j+2]
+                        if (xdif==0 and abs(ydif)==1) or (ydif==0 and abs(xdif)==1):
+                            notBranch = 1
+            if notBranch == 0:
+                branches.append((x,y))
+            else:
+                pseudobranches.append((x,y))
 
-    tips = []
-    branches = []
-    pseudobranches = []
-    for x in range(np.shape(maxis)[0]):
-        for y in range(np.shape(maxis)[1]):
-            if neighbours[x,y,0] == 1:
-                tips.append((x,y))
-            elif neighbours[x,y,0] == 3:
-                notBranch = 0
-                for i in range(3):
-                    for j in range(3):
-                        if i==j:
-                            pass
-                        else:
-                            xdif = neighbours[x,y,2*i+1]-neighbours[x,y,2*j+1]
-                            ydif = neighbours[x,y,2*i+2]-neighbours[x,y,2*j+2]
-                            if (xdif==0 and abs(ydif)==1) or (ydif==0 and abs(xdif)==1):
-                                notBranch = 1
-                if notBranch == 0:
-                    branches.append((x,y))
+maxLength = 0
+route = []
+widths = []
+
+#Track distances starting from each possible tip in the cell medial axis
+for t in tips:
+
+    lastPixel = t
+    currentPixel = (neighbours[lastPixel[0],lastPixel[1],1],neighbours[lastPixel[0],lastPixel[1],2])
+    length = sqrt((lastPixel[0]-currentPixel[0])**2+(lastPixel[1]-currentPixel[1])**2)
+    pixelsSinceLastBranch = [t]
+    blockedPixels = np.zeros(np.shape(bw))
+    route = [t,currentPixel]
+    widths = [maxis[t[0],t[1]],maxis[currentPixel[0],currentPixel[1]]]
+
+    while blockedPixels[t[0],t[1]] == 0:
+
+        if currentPixel in tips:
+
+            #pathLengths.append(length)
+            if length > maxLength:
+                maxLength = length
+                longestRoute = route
+                longestRouteWidths = widths
+            for pixel in pixelsSinceLastBranch:
+                blockedPixels[pixel[0],pixel[1]] = 1
+            pixelsSinceLastBranch.clear()
+            lastPixel = t
+            currentPixel = (neighbours[lastPixel[0],lastPixel[1],1],neighbours[lastPixel[0],lastPixel[1],2])
+            length = sqrt((lastPixel[0]-currentPixel[0])**2+(lastPixel[1]-currentPixel[1])**2)
+            pixelsSinceLastBranch = [t,currentPixel]
+            route = [t,currentPixel]
+            widths = [maxis[t[0],t[1]],maxis[currentPixel[0],currentPixel[1]]]
+
+        elif currentPixel in branches:
+            for i in range(4):
+                if i == 3:
+                    #set pixels back to last branch to blocked and return to tip
+                    for pixel in pixelsSinceLastBranch:
+                        blockedPixels[pixel[0],pixel[1]] = 1
+                    currentPixel = t
                 else:
-                    pseudobranches.append((x,y))
-
-    plt.imshow(bw+100*maxis,cmap="gray_r")
-
-    maxLength = 0
-    route = []
-    widths = []
-
-    #Track distances starting from each possible tip in the cell medial axis
-    for t in tips:
-
-        lastPixel = t
-        currentPixel = (neighbours[lastPixel[0],lastPixel[1],1],neighbours[lastPixel[0],lastPixel[1],2])
-        length = sqrt((lastPixel[0]-currentPixel[0])**2+(lastPixel[1]-currentPixel[1])**2)
-        pixelsSinceLastBranch = [t]
-        blockedPixels = np.zeros(np.shape(bw))
-        route = [t,currentPixel]
-        widths = [maxis[t[0],t[1]],maxis[currentPixel[0],currentPixel[1]]]
-
-        while blockedPixels[t[0],t[1]] == 0:
-
-            if currentPixel in tips:
-
-                #pathLengths.append(length)
-                if length > maxLength:
-                    maxLength = length
-                    longestRoute = route
-                    longestRouteWidths = widths
-                for pixel in pixelsSinceLastBranch:
-                    blockedPixels[pixel[0],pixel[1]] = 1
+                    pixelConsidered = (neighbours[currentPixel[0],currentPixel[1],2*i+1],neighbours[currentPixel[0],currentPixel[1],2*i+2])
+                    if pixelConsidered == lastPixel or blockedPixels[pixelConsidered[0],pixelConsidered[1]] == 1:
+                        pass
+                    else:
+                        newPixel = pixelConsidered
+                        break
+            if currentPixel == t:
+                #End this loop iteration if we have returned to the start
                 pixelsSinceLastBranch.clear()
                 lastPixel = t
                 currentPixel = (neighbours[lastPixel[0],lastPixel[1],1],neighbours[lastPixel[0],lastPixel[1],2])
@@ -105,71 +125,46 @@ def findLongestRoute(filename):
                 pixelsSinceLastBranch = [t,currentPixel]
                 route = [t,currentPixel]
                 widths = [maxis[t[0],t[1]],maxis[currentPixel[0],currentPixel[1]]]
-
-            elif currentPixel in branches:
-                for i in range(4):
-                    if i == 3:
-                        #set pixels back to last branch to blocked and return to tip
-                        for pixel in pixelsSinceLastBranch:
-                            blockedPixels[pixel[0],pixel[1]] = 1
-                        currentPixel = t
-                    else:
-                        pixelConsidered = (neighbours[currentPixel[0],currentPixel[1],2*i+1],neighbours[currentPixel[0],currentPixel[1],2*i+2])
-                        if pixelConsidered == lastPixel or blockedPixels[pixelConsidered[0],pixelConsidered[1]] == 1:
-                            pass
-                        else:
-                            newPixel = pixelConsidered
-                            break
-                if currentPixel == t:
-                    #End this loop iteration if we have returned to the start
-                    pixelsSinceLastBranch.clear()
-                    lastPixel = t
-                    currentPixel = (neighbours[lastPixel[0],lastPixel[1],1],neighbours[lastPixel[0],lastPixel[1],2])
-                    length = sqrt((lastPixel[0]-currentPixel[0])**2+(lastPixel[1]-currentPixel[1])**2)
-                    pixelsSinceLastBranch = [t,currentPixel]
-                    route = [t,currentPixel]
-                    widths = [maxis[t[0],t[1]],maxis[currentPixel[0],currentPixel[1]]]
-                    continue
-                else:
-                    length = length + sqrt((newPixel[0]-currentPixel[0])**2+(newPixel[1]-currentPixel[1])**2)
-                    lastPixel = currentPixel
-                    currentPixel = newPixel
-                    pixelsSinceLastBranch.clear()
-                    pixelsSinceLastBranch.append(currentPixel)
-                    route.append(currentPixel)
-                    widths.append(maxis[currentPixel[0],currentPixel[1]])
-
-            elif currentPixel in pseudobranches:
-                for i in range(3):
-                    pixelConsidered = (neighbours[currentPixel[0],currentPixel[1],2*i+1],neighbours[currentPixel[0],currentPixel[1],2*i+2])
-                    if pixelConsidered == lastPixel or pixelConsidered in pseudobranches:
-                        pass
-                    else:
-                        newPixel = pixelConsidered
-                        break
-                length = length + sqrt((newPixel[0]-currentPixel[0])**2+(newPixel[1]-currentPixel[1])**2)
-                lastPixel = currentPixel
-                currentPixel = newPixel
-                pixelsSinceLastBranch.append(currentPixel)
-                route.append(currentPixel)
-                widths.append(maxis[currentPixel[0],currentPixel[1]])
-
+                continue
             else:
-                for i in range(2):
-                    pixelConsidered = (neighbours[currentPixel[0],currentPixel[1],2*i+1],neighbours[currentPixel[0],currentPixel[1],2*i+2])
-                    if pixelConsidered == lastPixel:
-                        pass
-                    else:
-                        newPixel = pixelConsidered
                 length = length + sqrt((newPixel[0]-currentPixel[0])**2+(newPixel[1]-currentPixel[1])**2)
                 lastPixel = currentPixel
                 currentPixel = newPixel
+                pixelsSinceLastBranch.clear()
                 pixelsSinceLastBranch.append(currentPixel)
                 route.append(currentPixel)
                 widths.append(maxis[currentPixel[0],currentPixel[1]])
 
-    return route,widths
+        elif currentPixel in pseudobranches:
+            for i in range(3):
+                pixelConsidered = (neighbours[currentPixel[0],currentPixel[1],2*i+1],neighbours[currentPixel[0],currentPixel[1],2*i+2])
+                if pixelConsidered == lastPixel or pixelConsidered in pseudobranches:
+                    pass
+                else:
+                    newPixel = pixelConsidered
+                    break
+            length = length + sqrt((newPixel[0]-currentPixel[0])**2+(newPixel[1]-currentPixel[1])**2)
+            lastPixel = currentPixel
+            currentPixel = newPixel
+            pixelsSinceLastBranch.append(currentPixel)
+            route.append(currentPixel)
+            widths.append(maxis[currentPixel[0],currentPixel[1]])
 
+        else:
+            for i in range(2):
+                pixelConsidered = (neighbours[currentPixel[0],currentPixel[1],2*i+1],neighbours[currentPixel[0],currentPixel[1],2*i+2])
+                if pixelConsidered == lastPixel:
+                    pass
+                else:
+                    newPixel = pixelConsidered
+            length = length + sqrt((newPixel[0]-currentPixel[0])**2+(newPixel[1]-currentPixel[1])**2)
+            lastPixel = currentPixel
+            currentPixel = newPixel
+            pixelsSinceLastBranch.append(currentPixel)
+            route.append(currentPixel)
+            widths.append(maxis[currentPixel[0],currentPixel[1]])
+
+#Place longest axis route on image
 pathImage = np.zeros(np.shape(bw))
 xs = np.zeros(len(longestRoute))
 ys = np.zeros(len(longestRoute))
@@ -182,36 +177,38 @@ for i,pixel in enumerate(longestRoute[1:]):
     xs[i+1] = dist
     ys[i+1] = longestRouteWidths[i+1]
 
-fig,ax = plt.subplots(2,2,figsize=(12,12))
-ax[0,0].imshow(originalimage,cmap="gray_r")
-ax[0,1].imshow((bw+100*maxis),cmap="gray_r")
-ax[1,0].imshow((bw+1000*pathImage),cmap="gray_r")
-ax[1,1].plot(xs,ys,label="Raw data")
-z = np.polyfit(xs,ys,3)
-z[-1] = 0
-zfun = np.poly1d(z)
-zs = zfun(xs)
-ax[1,1].plot(xs,zs,label="Cubic")
-z = np.polyfit(xs,ys,2)
-z[-1] = 0
-zfun = np.poly1d(z)
-zs = zfun(xs)
-ax[1,1].plot(xs,zs,label="Quadratic")
 
+#%%
 
+# Adjust distances along axis such that widest point is at 0
+# Therefore fitted mean value is always around 0
+peakdist = xs[np.argmax(longestRouteWidths)]
+xs = xs - peakdist
+
+#Fit Gaussian to width data
 fitter = modeling.fitting.LevMarLSQFitter()
 model = modeling.models.Gaussian1D()   # depending on the data you need to give some initial values
 fitted_model = fitter(model, xs, ys)
-ax[1,1].plot(xs,fitted_model(xs),label="Gaussian")
 
-ax[1,1].legend(loc="best")
-ax[0,0].tick_params(axis='x', which='both', bottom=False, top=False, labelbottom=False)
-ax[0,0].tick_params(axis='y', which='both', left=False, right=False, labelleft=False)
-ax[0,1].tick_params(axis='x', which='both', bottom=False, top=False, labelbottom=False)
-ax[0,1].tick_params(axis='y', which='both', left=False, right=False, labelleft=False)
-ax[1,0].tick_params(axis='x', which='both', bottom=False, top=False, labelbottom=False)
-ax[1,0].tick_params(axis='y', which='both', left=False, right=False, labelleft=False)
+
+#Plot results
+fig,ax = plt.subplots(1,2,figsize=(12,6))
+ax[0].imshow((bw+1000*pathImage),cmap="gray_r")
+ax[1].plot(xs,ys,label="Raw data")
+ax[1].plot(xs,fitted_model(xs),label="Gaussian")
+ax[1].set_xlabel("Axis distance")
+ax[1].set_ylabel("Width")
+ax[1].legend(loc="best")
+ax[0].tick_params(axis='x', which='both', bottom=False, top=False, labelbottom=False)
+ax[0].tick_params(axis='y', which='both', left=False, right=False, labelleft=False)
 fig.savefig(argv[1][:-4]+"Processed.png",bbox_inches='tight',padding_inches=0,dpi=300)
-#plt.show()
 
+#Save fit values to file
+outfile = open(argv[1][:-4]+"Parameters.txt","w")
+outfile.write(str(fitted_model.amplitude.value)+"\n")
+outfile.write(str(fitted_model.mean.value)+"\n")
+outfile.write(str(fitted_model.stddev.value)+"\n")
+outfile.close()
+
+print(fitted_model)
 #%%
